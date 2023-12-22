255c530ce1775b42f887d69d23fcce8067b3ec6d
Livia Souza <liviaa.sa@hotmail.com>
2023-12-21 22:42:04 -0300

    routes updated
    sessions, brand and logo deleted

--- src/footer.routes.js
+++ src/footer.routes.js
@@ -1,6 +1,6 @@
 // @mui icons
-import FacebookIcon from "@mui/icons-material/Facebook";
-import TwitterIcon from "@mui/icons-material/Twitter";
+//import FacebookIcon from "@mui/icons-material/Facebook";
+//import TwitterIcon from "@mui/icons-material/Twitter";
 //import GitHubIcon from "@mui/icons-material/GitHub";
 //import YouTubeIcon from "@mui/icons-material/YouTube";
 
@@ -8,54 +8,14 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 import MKTypography from "components/MKTypography";
 
 // Images
-import logoCT from "assets/images/logogui.png";
+//import logoCT from "assets/images/logogui.png";
 
 const date = new Date().getFullYear();
 
 export default {
-  brand: {
-    name: "Clínica Braz",
-    image: logoCT,
-    route: "/",
-  },
-  socials: [
-    {
-      icon: <FacebookIcon />,
-      link: "https://www.facebook.com/dr.guilhermebraz",
-    },
-    {
-      icon: <TwitterIcon />,
-      link: "https://twitter.com/",
-    },
-  ],
-  menus: [
-    {
-      name: "company",
-      items: [
-        { name: "about us", href: "https://www.creative-tim.com/presentation" },
-        { name: "freebies", href: "https://www.creative-tim.com/templates/free" },
-        { name: "premium tools", href: "https://www.creative-tim.com/templates/premium" },
-        { name: "blog", href: "https://www.creative-tim.com/blog" },
-      ],
-    },
-    {
-      name: "resources",
-      items: [
-        { name: "illustrations", href: "https://iradesign.io/" },
-        { name: "bits & snippets", href: "https://www.creative-tim.com/bits" },
-        { name: "affiliate program", href: "https://www.creative-tim.com/affiliates/new" },
-      ],
-    },
-    {
-      name: "help & support",
-      items: [
-        { name: "contact us", href: "https://www.creative-tim.com/contact-us" },
-        { name: "knowledge center", href: "https://www.creative-tim.com/knowledge-center" },
-        { name: "custom development", href: "https://services.creative-tim.com/" },
-        { name: "sponsorships", href: "https://www.creative-tim.com/sponsorships" },
-      ],
-    },
-  ],
+  brand: {},
+  socials: [],
+  menus: [],
   copyright: (
     <MKTypography variant="button" fontWeight="regular">
       All rights reserved. Copyright &copy; {date} Feito com amor por{" "}
